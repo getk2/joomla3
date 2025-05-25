ff947abeb3d3b8f2eb7ce53a128f5ee0568b2c51
Fotis Evangelou <fevangelou@users.noreply.github.com>
2025-05-25 22:23:19 +0300

    Check for headers_sent()

--- libraries/src/Application/WebApplication.php
+++ libraries/src/Application/WebApplication.php
@@ -1302,7 +1302,7 @@ class WebApplication extends BaseApplication
 		$this->set('uri.base.path', $path . '/');
 
 		// Set the extended (non-base) part of the request URI as the route.
-		if (stripos($this->get('uri.request'), $this->get('uri.base.full')) === 0)
+		if ($this->get('uri.request') !== null && stripos($this->get('uri.request'), $this->get('uri.base.full')) === 0)
 		{
 			$this->set('uri.route', substr_replace($this->get('uri.request'), '', 0, strlen($this->get('uri.base.full'))));
 		}
